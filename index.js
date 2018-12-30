9de01972a487d3b550e6e4d1f3ccea5088c6c627
Rossev Gleb <rossevgleb@gmail.com>
2018-12-31 01:16:16 +0300

    Two middleware

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  const { url } = req;
-  const { method } = req;
+const app = express();
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-
-    return res.end();
-  }
-  if (url === "/" && method === "POST") {
-    fs.writeFileSync("create-user.txt", "DUMMY");
-    return res.end();
-  }
-  if (url === "/create-user") {
-    const body = [];
-    req.on("data", chunk => {
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const bodyFromBuffer = Buffer.concat(body).toString();
-
-      console.log(bodyFromBuffer);
-    });
-  }
-
-  if (url === "/users") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<ul><li>Users</li></ul>");
-    return res.end();
-  }
+app.use((req, res, next) => {
+  console.log("First log");
+  next();
 });
-console.log("Server is running");
-server.listen(4000);
+
+app.use((req, res, next) => {
+  console.log("Second log");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send(new Date());
+});
+
+app.listen(3000);
